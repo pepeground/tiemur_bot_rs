54f0f1e54af61f16870477b99360ddaeb3bf268d
Marat Safin <jeizsm@gmail.com>
2017-09-12 17:27:08 +0300

    add top tiemurs

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -2,7 +2,8 @@ mod response;
 
 use std::env;
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{RefCell, RefMut};
+use std::collections::BinaryHeap;
 
 use telegram_bot::{Api, CanReplySendMessage, CanGetFile};
 use telegram_bot::types::{Message, MessageKind};
@@ -14,7 +15,7 @@ use hyper_tls::HttpsConnector;
 use hyper::error::UriError;
 use img_hash::{ImageHash, HashType};
 use image::load_from_memory as load;
-use rocksdb::{DB, Options, IteratorMode};
+use rocksdb::{DB, Options, IteratorMode, ColumnFamily};
 use bincode::{serialize, deserialize, Infinite};
 
 use types::{Image,User,UserContent};
@@ -27,12 +28,12 @@ pub fn process(message: Rc<Message>,
     let clone = message.clone();
     let clone1 = message.clone();
 
-    let id = &message.chat.id().to_string();
-    let cf_handle = db.borrow().cf_handle(&id);
-    let cf = match cf_handle {
-        Some(cf) => cf,
-        None => db.borrow_mut().create_cf(&id, &Options::default()).unwrap(),
-    };
+
+    let mut cf_name = message.chat.id().to_string();
+
+    let image_cf = db_handle(db.borrow_mut(), &cf_name);
+    cf_name.push_str("_users");
+    let user_cf = db_handle(db.borrow_mut(), &cf_name);
 
     match message.kind {
         MessageKind::Photo { ref data, .. } => {
@@ -50,16 +51,16 @@ pub fn process(message: Rc<Message>,
                 .and_then(move |hash| {
                     let db = db.borrow();
                     let bytes = &hash.bitv.to_bytes()[..];
-                    let find = db.iterator_cf(cf, IteratorMode::End)
+                    let find = db.iterator_cf(image_cf, IteratorMode::End)
                         .unwrap()
                         .find(|&(ref key, ref _value)| key.as_ref() == bytes);
-                    let telegram_user = clone.from.clone().unwrap();
+                    let telegram_user = clone.from.clone().ok_or("user empty".to_string())?;
                     let mut user: User = telegram_user.into();
                     let key = serialize(&user.0, Infinite).unwrap();
-                    let user_row = db.get_cf(cf, &key).unwrap();
+                    let user_row = db.get_cf(user_cf, &key).unwrap();
                     if user_row.is_none() {
                         let value = serialize(&user.1, Infinite).unwrap();
-                        let _ = db.put_cf(cf, &key, &value);
+                        let _ = db.put_cf(user_cf, &key, &value);
                     }
                     match find {
                         Some((_key, value)) => {
@@ -71,20 +72,20 @@ pub fn process(message: Rc<Message>,
                                     ..user.1
                                 };
                                 let value = serialize(&user.1, Infinite).unwrap();
-                                let _ = db.put_cf(cf, &key, &value);
+                                let _ = db.put_cf(user_cf, &key, &value);
                             }
-                            Ok((image, user))
+                            Ok((image, user.1))
                         }
                         None => {
                             let image = Image::new(clone.id, user.0, clone.date);
                             let value = serialize(&image, Infinite).unwrap();
-                            let _ = db.put_cf(cf, bytes, &value);
+                            let _ = db.put_cf(image_cf, bytes, &value);
                             Err("new record".to_string())
                         }
                     }
                 })
                 .and_then(move |(image, user)| {
-                    let text = response::build(&image, &user.1, &clone1.chat);
+                    let text = response::build(&image, &user, &clone1.chat);
                     api.send(clone1.text_reply(text))
                         .map_err(|e| e.to_string())
                 });
@@ -92,6 +93,31 @@ pub fn process(message: Rc<Message>,
                 future.map_err(|_| ()).map(|_| ())
             })
         }
-        _ => println!("{:?}", message),
+        MessageKind::Text { ref data, .. } => {
+            match data.as_ref() {
+                "/tiemur_stats" | "/tiemur_stats@TiemurBot" => {
+                    let mut users: BinaryHeap<_> = db.borrow()
+                        .iterator_cf(user_cf, IteratorMode::End)
+                        .unwrap()
+                        .map(|(_key,value)| -> UserContent {deserialize(&*value).unwrap()})
+                        .collect();
+                    let text = users.pop().map_or("".to_string(), |a| a.first_name);
+                    let future = api.send(clone1.text_reply(text))
+                        .map_err(|e| e.to_string());
+                    handle.spawn({
+                        future.map_err(|_| ()).map(|_| ())
+                    })
+                }
+                _ => ()
+            }
+        }
+        _ => ()
+    }
+}
+
+fn db_handle(mut db: RefMut<DB>, cf_name: &str) -> ColumnFamily {
+    match db.cf_handle(cf_name) {
+        Some(cf) => cf,
+        None => db.create_cf(cf_name, &Options::default()).unwrap(),
     }
 }
