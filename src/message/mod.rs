b474f132465384977453b04b80733a6ab354cd27
Marat Safin <jeizsm@gmail.com>
2017-09-14 08:20:47 +0300

    refactor

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -2,7 +2,7 @@ mod response;
 
 use std::env;
 use std::rc::Rc;
-use std::cell::{RefCell, RefMut};
+use std::cell::RefCell;
 use std::collections::BinaryHeap;
 
 use telegram_bot::{Api, CanReplySendMessage, CanGetFile};
@@ -25,15 +25,12 @@ pub fn process(message: Rc<Message>,
                handle: &Handle,
                client: Client<HttpsConnector<HttpConnector>>,
                db: Rc<RefCell<DB>>) {
-    let clone = message.clone();
-    let clone1 = message.clone();
-
+    let message_clone = message.clone();
 
     let mut cf_name = message.chat.id().to_string();
-
-    let image_cf = db_handle(db.borrow_mut(), &cf_name);
+    let image_cf = db_handle(&mut db.borrow_mut(), &cf_name);
     cf_name.push_str("_users");
-    let user_cf = db_handle(db.borrow_mut(), &cf_name);
+    let user_cf = db_handle(&mut db.borrow_mut(), &cf_name);
 
     match message.kind {
         MessageKind::Photo { ref data, .. } => {
@@ -54,7 +51,7 @@ pub fn process(message: Rc<Message>,
                     let find = db.iterator_cf(image_cf, IteratorMode::End)
                         .unwrap()
                         .find(|&(ref key, ref _value)| key.as_ref() == bytes);
-                    let telegram_user = clone.from.clone().ok_or("user empty".to_string())?;
+                    let telegram_user = message_clone.from.clone().ok_or("user empty".to_string())?;
                     let mut user: User = telegram_user.into();
                     let key = serialize(&user.0, Infinite).unwrap();
                     let user_row = db.get_cf(user_cf, &key).unwrap();
@@ -74,19 +71,19 @@ pub fn process(message: Rc<Message>,
                                 let value = serialize(&user.1, Infinite).unwrap();
                                 let _ = db.put_cf(user_cf, &key, &value);
                             }
-                            Ok((image, user.1))
+                            Ok((message_clone, image, user.1))
                         }
                         None => {
-                            let image = Image::new(clone.id, user.0, clone.date);
+                            let image = Image::new(message_clone.id, user.0, message_clone.date);
                             let value = serialize(&image, Infinite).unwrap();
                             let _ = db.put_cf(image_cf, bytes, &value);
                             Err("new record".to_string())
                         }
                     }
                 })
-                .and_then(move |(image, user)| {
-                    let text = response::build(&image, &user, &clone1.chat);
-                    api.send(clone1.text_reply(text))
+                .and_then(move |(message_clone, image, user)| {
+                    let text = response::build(&image, &user, &message_clone.chat);
+                    api.send(message_clone.text_reply(text))
                         .map_err(|e| e.to_string())
                 });
             handle.spawn({
@@ -102,7 +99,7 @@ pub fn process(message: Rc<Message>,
                         .map(|(_key,value)| -> UserContent {deserialize(&*value).unwrap()})
                         .collect();
                     let text = users.pop().map_or("".to_string(), |a| a.first_name);
-                    let future = api.send(clone1.text_reply(text))
+                    let future = api.send(message_clone.text_reply(text))
                         .map_err(|e| e.to_string());
                     handle.spawn({
                         future.map_err(|_| ()).map(|_| ())
@@ -115,7 +112,7 @@ pub fn process(message: Rc<Message>,
     }
 }
 
-fn db_handle(mut db: RefMut<DB>, cf_name: &str) -> ColumnFamily {
+fn db_handle(db: &mut DB, cf_name: &str) -> ColumnFamily {
     match db.cf_handle(cf_name) {
         Some(cf) => cf,
         None => db.create_cf(cf_name, &Options::default()).unwrap(),
